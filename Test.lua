5e20da4fc32f7cca52c71445c1c5dcbc2d3f4715
Erennzx <154311175+Erennzx@users.noreply.github.com>
2025-08-24 17:03:47 +0300

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,6 +1,19 @@
-local Utility = sharedRequire('@utils/Utility.lua');
-local createBaseESP = sharedRequire('@utils/createBaseESP.lua');
-local library = sharedRequire('@UILibrary.lua');
+local function sharedRequire(url)
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(url))()
+    end)
+    if not success then
+        error("Failed to load shared module: " .. url)
+    end
+    return result
+end
+
+
+-- ... diğer kodlar ...
+
+local Utility = sharedRequire('https://raw.githubusercontent.com/Erennzx/aztup/refs/heads/main/files/utils/Utility.lua');
+local createBaseESP = sharedRequire('https://raw.githubusercontent.com/Erennzx/aztup/refs/heads/main/files/utils/createBaseESP.lua');
+local library = sharedRequire('https://raw.githubusercontent.com/Erennzx/aztup/refs/heads/main/files/UILibrary.lua');
 
 local partsESP = createBaseESP('npcs');
 
@@ -39,5 +52,6 @@ function Utility:renderOverload(data)
         flag = 'Npcs'
     })
 end;
+
 
 Utility.listenToChildAdded(workspace.Parts, onPartAdded);
